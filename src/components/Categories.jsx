2363b95205dd11272268d7f86217f1eb154a40cb
Garry <igorprohorchenko@gmail.com>
2022-08-02 16:49:09 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const Categories = () => {
+  return (
+    <div className="categories">
+    <ul>
+      <li className="active">Все</li>
+      <li>Ботинки</li>
+      <li>Полуботинки</li>
+      <li>Кеды</li>
+      <li>Кроссовки</li>
+      <li>Люферы</li>
+      <li>Сланцы</li>
+    </ul>
+  </div>
+  )
+}
+export default Categories;
---
+++ src/components/ShoeBlock.jsx
@@ -0,0 +1,482 @@
+import React from 'react';
+
+const ShoeBlock = () =>{
+  return (
+    <>
+    <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/09721711/09721711_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Ботинки O.LIVE naturalle</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">39</li>
+        <li>40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 150 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/1000261/1000261_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Ботинки X-Plode</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 140 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/091132512/091132512_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Полуботинки Go.Do.</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 86 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/09720770/09720770_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Полуботинки O.LIVE</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">39</li>
+        <li>40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 130 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/100006/100006_1_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Кеды O.LIVE naturalle</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">39</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>45</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 140 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/058301463/058301463_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title"> Кеды X-Plode </h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 150 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/0583116731/0583116731_1_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Кроссовки X-Plode</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">38</li>
+        <li>39</li>
+        <li>40</li>
+        <li>41</li>
+        <li>42</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 120 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/1303180711/1303180711_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Кроссовки X-Plode</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 150 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div> 
+  <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/078518035/078518035_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Сланцы O.LIVE naturalle</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">39</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+        <li>45</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 86 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div>
+  <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/09722207/09722207_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Лоферы Go.Do.</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+        <li>45</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 100 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div>
+  <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/097265511/097265511_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Лоферы O.LIVE naturalle</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 120 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div>
+  <div className="shoes-block">
+    <img
+      className="shoes-block__image"
+      src="https://img.megatop.by/photo/09722114/09722114_thumb_320.webp"
+      alt="Shoes"
+    />
+    <h4 className="shoes-block__title">Лоферы Go.Do.</h4>
+    <div className="shoes-block__selector">
+      <ul>
+        <li className="active">обычная</li>
+        <li>по коду ЖАРА</li>
+      </ul>
+      <ul>
+        <li className="active">39</li>
+        <li>40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+      </ul>
+    </div>
+    <div className="shoes-block__bottom">
+      <div className="shoes-block__price">от 80 Br</div>
+      <div className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Добавить</span>
+        <i>2</i>
+      </div>
+    </div>
+  </div>
+  </>
+  );
+}
+export default ShoeBlock;
