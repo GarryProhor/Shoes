264fec9c19f02099f2a7c1d95ec934c6f3fcef8f
GarryProhor <84273694+GarryProhor@users.noreply.github.com>
2022-08-07 10:29:32 +0300

    add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/ShoeBlock/Skeleton";
 import ShoeBlock from "../components/ShoeBlock";
+import Pagination from "../components/Pagination";
 
 const Home = ({searchValue}) => {
     const [shoes, setShoes] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [activeCategories, setActiveCategories] = React.useState(0);
+    const [currentPage, setCurrentPage] = React.useState(1);
     const [activeSort, setActiveSort] = React.useState({
         name: 'популярности',
         sortProperty: 'rating'
@@ -20,7 +22,7 @@ const Home = ({searchValue}) => {
         const sortBy = activeSort.sortProperty.replace('-','')//удаляем "-" из запроса
         const cat = activeCategories > 0 ? `category=${activeCategories}` : ``//выбираеи категорию
 
-        fetch(`https://62da700d9eedb699636e2d90.mockapi.io/shoes?${cat}&sortBy=${sortBy}&order=${order}`)
+        fetch(`https://62da700d9eedb699636e2d90.mockapi.io/shoes?page=${currentPage}&limit=4&${cat}&sortBy=${sortBy}&order=${order}`)
             .then(response => response.json())
             .then(shoe=>
             {
@@ -28,7 +30,7 @@ const Home = ({searchValue}) => {
                 setIsLoading(false);
             })
         window.scroll(0,0);
-    },[activeCategories, activeSort]);
+    },[activeCategories, activeSort, currentPage]);
 
 
     const skeleton = [...new Array(6)].map((_, index)=><Skeleton key={index}/>)
@@ -51,6 +53,7 @@ const Home = ({searchValue}) => {
             <div className="content__items">
                 {isLoading ? skeleton : shoeses}
             </div>
+            <Pagination currentPage={currentPage} onChangePage={(page)=>setCurrentPage(page)}/>
         </>
     )
 }
